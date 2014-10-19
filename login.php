c3b09742a6ce9c393d6692552a73364c9b4bc3ca
John Arce <johna@bixly.com>
2014-10-20 07:54:24 +0800

    changed API keys

--- login.php
+++ login.php
@@ -2,8 +2,8 @@
 define(CAPTCHA_API, "http://captchavisum.bixly.com/api/");
 define(CHALLENGE_API, CAPTCHA_API . "challenge/");
 define(VERIFY_API, CAPTCHA_API . "verify/");
-define(API_KEY, "db48aec447e24caaa8fed3e64764f90f");
-define(PRIVATE_KEY, "a2f0edab6a7941e38dcc65898a06fabc");
+define(API_KEY, "6dd4075bd4634f86a42ae648c7e5c59e");
+define(PRIVATE_KEY, "dbc551f4b231459daaef459cc4441bd2");
 
 session_start();//starting session
 $error=''; //variable to store error message
@@ -12,14 +12,14 @@ if (isset($_POST['submit'])) {
     if (empty($_POST['username']) || empty($_POST['password'])) {
         $error = "Username or Password is invalid"; 
     }
-    else {                            
+    else {
         // prepare post variables
         $username=$_POST['username'];
         $password=$_POST['password'];
         $challenge=$_POST['challenge'];
         $site=$_POST['site'];
         $challenge_response=$_POST['challenge_response'];
-        
+
         // verify challenge: post request
         $post  = array (
             'challenge' => $challenge,
@@ -46,7 +46,7 @@ if (isset($_POST['submit'])) {
             // login user here
             $_SESSION['login_user']=$username;//Initializing Session
             header("location: profile.php");//Redirecting to other page 
-        }                       
+        }
     }
 }
 ?>
