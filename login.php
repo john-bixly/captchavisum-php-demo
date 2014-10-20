8d538d65a08331bea23f561333869d231a0c3445
John Arce <johna@bixly.com>
2014-10-21 05:14:50 +0800

    add IP detection and code cleanup

--- login.php
+++ login.php
@@ -5,6 +5,8 @@ define(VERIFY_API, CAPTCHA_API . "verify/");
 define(API_KEY, "6dd4075bd4634f86a42ae648c7e5c59e");
 define(PRIVATE_KEY, "dbc551f4b231459daaef459cc4441bd2");
 
+include('utils.php');
+
 session_start();//starting session
 $error=''; //variable to store error message
 
@@ -25,17 +27,9 @@ if (isset($_POST['submit'])) {
             'challenge' => $challenge,
             'challenge_response' => $challenge_response,
             'private_key' => PRIVATE_KEY,
-            'remote_ip' => '127.0.0.1', //pass client's IP
+            'remote_ip' => get_ip(), //pass client's IP
         );
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, VERIFY_API);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $result = curl_exec ($ch);
-
-        curl_close ($ch);
+        $result = verify_api($post);
 
         // if failed
         if ($result!='true') {
