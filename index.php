a9812f6e5270eb3df56499090aba1b259a44d451
John Arce <johna@bixly.com>
2014-10-23 07:52:29 +0800

    change div ID

--- index.php
+++ index.php
@@ -18,9 +18,9 @@ include('login.php');//includes login script
                 <input type="text" name="username" id="name" placeholder="username"/><br /><br />
                 <label>Password  :</label>
                 <input type="password" name="password" id="password" placeholder="**********"/><br/><br />
-                <!-- required div element with id 'captchavisum_form' 
+                <!-- required div element with id 'captchavisum_widget_div' 
                 where the challenge question will be placed -->
-                <div id="captchavisum_form"></div>
+                <div id="captchavisum_widget_div"></div>
                 <input type="submit" value=" Login " name="submit"/><br />
                 <span><?php echo $error; ?></span>
             </form>
